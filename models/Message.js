a230df47c9e7c53ff20915589026a3cd0fc1dfc8
joydotcom <msjoyjackson@gmail.com>
2021-04-28 14:05:34 -0400

    added game and console id to message model

--- models/Message.js
+++ models/Message.js
@@ -30,6 +30,20 @@ Message.init(
         key: 'id',
       },
     },
+    game_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'game',
+          key: 'id',
+        },
+    },
+    console_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'console',
+          key: 'id',
+        },
+    },
   },
   {
     sequelize,
