f82e4b0f79070ed8a0ad55a03e3ac9633728cdcb
Green Kim <greenkimparsons@gmail.com>
2021-05-02 10:35:33 -0400

    moved login button to the right

--- public/js/login.js
+++ public/js/login.js
@@ -11,18 +11,18 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    // const response = await fetch("/api/users/login", {
-    //   method: "POST",
-    //   body: JSON.stringify({ email, password }),
-    //   headers: { "Content-Type": "application/json" },
-    // });
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-    // if (response.ok) {
-    //   // If successful, redirect the browser to the profile page
-    //   document.location.replace("/");
-    // } else {
-    //   alert(response.statusText);
-    // }
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
   }
 };
 
