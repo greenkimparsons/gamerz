8af08597a40260676fb3c2bbace1cc7f5aee0f46
joydotcom <msjoyjackson@gmail.com>
2021-05-01 22:53:31 -0400

    message stuff

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,49 +1,54 @@
-// DOM VARIABLES
-var noteFormEl = $("#note-form")
-var noteInputEl = $("#note-input")
-var savedNotesEl = $("#saved-notes")
-var clearButtonEl = $("#clear-button")
 
 
-var notesArray = []
-// when submit is clicked, we run this function that prevents default, 
-// adds user input to an array called notesArray and sets it to local storage.
-// it also runs the function get list
 
-noteFormEl.on("submit", function (event) {
-    notesArray = JSON.parse(localStorage.getItem("notesArray")) || [];
-    event.preventDefault();
-    var noteInput = noteInputEl.val();
-    notesArray.push(noteInput);
-    localStorage.setItem("notesArray", JSON.stringify(notesArray))
-    noteInputEl.val('');
 
-    getList()
+// // DOM VARIABLES
+// var noteFormEl = $("#note-form")
+// var noteInputEl = $("#note-input")
+// var savedNotesEl = $("#saved-notes")
+// var clearButtonEl = $("#clear-button")
 
-})
 
-clearButtonEl.click(function (event) {
-  event.preventDefault();
-  localStorage.removeItem("notesArray");
-  getList()
-})
+// var notesArray = []
+// // when submit is clicked, we run this function that prevents default, 
+// // adds user input to an array called notesArray and sets it to local storage.
+// // it also runs the function get list
 
-// get list gets the value of notesArray from storage, iterates through the list, 
-// and appends each item to savedNotesP, which is then appended to savedNotes
-function getList() {
+// noteFormEl.on("submit", function (event) {
+//     notesArray = JSON.parse(localStorage.getItem("notesArray")) || [];
+//     event.preventDefault();
+//     var noteInput = noteInputEl.val();
+//     notesArray.push(noteInput);
+//     localStorage.setItem("notesArray", JSON.stringify(notesArray))
+//     noteInputEl.val('');
 
-    if (localStorage.getItem("notesArray")) {
-        notesArray = JSON.parse(localStorage.getItem("notesArray")) || [];
-        savedNotesEl.empty();
-        for (i = 0; i < notesArray.length; i++) {
-            var savedNotesP = $("<ul>")
-            notesLi = $("<li>").text(notesArray[i])
-            savedNotesP.append(notesLi)
-            savedNotesEl.append(savedNotesP)
-        }
-    } else {
-      savedNotesEl.empty();
-    }
-}
+//     getList()
 
-getList();
+// })
+
+// clearButtonEl.click(function (event) {
+//   event.preventDefault();
+//   localStorage.removeItem("notesArray");
+//   getList()
+// })
+
+// // get list gets the value of notesArray from storage, iterates through the list, 
+// // and appends each item to savedNotesP, which is then appended to savedNotes
+// function getList() {
+
+//     if (localStorage.getItem("notesArray")) {
+//         notesArray = JSON.parse(localStorage.getItem("notesArray")) || [];
+//         savedNotesEl.empty();
+//         for (i = 0; i < notesArray.length; i++) {
+//             var savedNotesP = $("<ul>")
+//             notesLi = $("<li>").text(notesArray[i])
+//             savedNotesP.append(notesLi)
+//             savedNotesEl.append(savedNotesP)
+//         }
+//     } else {
+//       savedNotesEl.empty();
+//     }
+// }
+
+// getList();
+
