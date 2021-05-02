4d44eb4d6297781b623e6097f547ddd312d43e40
Nicholas Ganim <nicog99@gmail.com>
2021-05-01 21:12:33 -0400

    login page logic

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -74,10 +74,10 @@ router.get('/loggedin', withAuth, async (req, res) => {
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   res.redirect('/');
+  //   return;
+  // }
 
   res.render('login');
 });
