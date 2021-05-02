63dd06f3927eaae2e349e12e6c67080a1def5b0d
joydotcom <msjoyjackson@gmail.com>
2021-05-01 21:53:45 -0400

    added comments section

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,27 +1,49 @@
-const commentFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    const comment = document.querySelector('#comment').value.trim();
-    
-  
-    if (comment) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/message', {
-        method: 'POST',
-        body: JSON.stringify({ comment }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-    //   if (response.ok) {
-    //     // If successful, redirect the browser to the profile page
-    //     document.location.replace('/');
-    //   } else {
-    //     alert(response.statusText);
-    //   }
-    // }
-  }};
+// DOM VARIABLES
+var noteFormEl = $("#note-form")
+var noteInputEl = $("#note-input")
+var savedNotesEl = $("#saved-notes")
+var clearButtonEl = $("#clear-button")
 
-document
-.querySelector('.comment')
-.addEventListener('submit', loginFormHandler);
+
+var notesArray = []
+// when submit is clicked, we run this function that prevents default, 
+// adds user input to an array called notesArray and sets it to local storage.
+// it also runs the function get list
+
+noteFormEl.on("submit", function (event) {
+    notesArray = JSON.parse(localStorage.getItem("notesArray")) || [];
+    event.preventDefault();
+    var noteInput = noteInputEl.val();
+    notesArray.push(noteInput);
+    localStorage.setItem("notesArray", JSON.stringify(notesArray))
+    noteInputEl.val('');
+
+    getList()
+
+})
+
+clearButtonEl.click(function (event) {
+  event.preventDefault();
+  localStorage.removeItem("notesArray");
+  getList()
+})
+
+// get list gets the value of notesArray from storage, iterates through the list, 
+// and appends each item to savedNotesP, which is then appended to savedNotes
+function getList() {
+
+    if (localStorage.getItem("notesArray")) {
+        notesArray = JSON.parse(localStorage.getItem("notesArray")) || [];
+        savedNotesEl.empty();
+        for (i = 0; i < notesArray.length; i++) {
+            var savedNotesP = $("<ul>")
+            notesLi = $("<li>").text(notesArray[i])
+            savedNotesP.append(notesLi)
+            savedNotesEl.append(savedNotesP)
+        }
+    } else {
+      savedNotesEl.empty();
+    }
+}
+
+getList();
